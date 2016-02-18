5937760f2fd4507443d14264eb90788df823640a
tophgg <395886828@qq.com>
2016-02-18 17:07:37 +0800

    ueditor

--- hotel/Hotel/Admin/Controller/NewsListController.class.php
+++ hotel/Hotel/Admin/Controller/NewsListController.class.php
@@ -3,12 +3,12 @@ namespace Admin\Controller;
 use Think\Controller;
 class NewsListController extends Controller {
 
-    private $newsList;
-    private $activityList;
+    public $newsList;
+    public $activityList;
     public function __construct(){
         parent::__construct();
-        $this->$newsList = M('NewsList');
-        // $this->$activityList = M('ActivityList');
+        $this->newsList=D('NewsList');
+        // $this->activityList = M('ActivityList');
     }
     
 /**
@@ -16,8 +16,8 @@ class NewsListController extends Controller {
  * @return [type] [description]
  */
 	public function newsList(){
-        $result = $this->M('NewsList')->showNews();
-        print_r($result);exit;
+        $result = $this->newsList->showNews();
+        // print_r($result);exit;
 		if($result){
             $this->assign('result',$result);
         }else{
@@ -26,27 +26,21 @@ class NewsListController extends Controller {
 		$this->display();
 	}    
 	//添加行业资讯
-	public function addNews(){//添加公告
+	public function addNews(){
         if(IS_POST){
             $data=I();
-            $data['type']=1;//type:1-通知公告
-            $data['author']=I('author');
-            $data['title']=I('title');
-            $data['intro']=I('intro');
-            $data['content']=I('content');
-            $data['addtime'] =time();
-            $data['updatetime'] =time();
             if($data['title']==''){
                 $this->ajaxReturn(array('status'=>0,'message'=>'请填写标题！'));
             }
-            if($data['intro']==''){
-                $this->ajaxReturn(array('status'=>0,'message'=>'请填写通知公告简介！'));
+            if($data['details']==''){
+                $this->ajaxReturn(array('status'=>0,'message'=>'请填写新闻详情！'));
             }
-            if($data['content']==''){
-                $this->ajaxReturn(array('status'=>0,'message'=>'请填写通知公告内容！'));
-            }
-            if($notice->create() && $data['intro']!='' && $data['title']!=''){
-                $notice->add($data);
+            // if($data['date']==''){
+            //     $this->ajaxReturn(array('status'=>0,'message'=>'请填写新闻日期！'));
+            // }
+            $result = $this->newsList->addN($data);
+            // print_r($result);
+            if($result){
                 $this->ajaxReturn(array('status'=>1, 'message'=>'通知公告添加成功！')); 
             }else{
                 $this->ajaxReturn(array('status'=>0, 'message'=>'通知公告添加失败！')); 
@@ -56,50 +50,41 @@ class NewsListController extends Controller {
         }
 	}
 	//编辑行业资讯
-	public function editNews($id=''){//编辑通知公告
+	public function editNews($id=''){
 		if (IS_POST) {//获取信息
-            $data=I();
-            $data['type']=1;//type:1-通知公告
-            $data['author']=I('author');
-            $data['title']=I('title');
-            $data['intro']=I('intro');
-            $data['content']=I('content');
-            $data['updatetime'] =strtotime(I('updatetime'));
-            //$data['updatetime'] =time();
+            $data = I();
+            if($id == ''){
+                $this->ajaxReturn(array('status'=>0,'message'=>'获取新闻id失败！'));
+            }
             if($data['title']==''){
                 $this->ajaxReturn(array('status'=>0,'message'=>'请填写标题！'));
             }
-            if($data['intro']==''){
-                $this->ajaxReturn(array('status'=>0,'message'=>'请填写通知公告简介！'));
+            if($data['details']==''){
+                $this->ajaxReturn(array('status'=>0,'message'=>'请填写新闻详情！'));
             }
-            if($data['content']==''){
-                $this->ajaxReturn(array('status'=>0,'message'=>'请填写通知公告内容！'));
-            }
-            if($notice->create() && $data['intro']!='' && $data['title']!=''){
-                $notice->save($data);
+            // if($data['date']==''){
+            //     $this->ajaxReturn(array('status'=>0,'message'=>'请填写新闻日期！'));
+            // }
+            $result = $this->newsList->editN($id,$data);
+            if($result){
                 $this->ajaxReturn(array('status'=>1, 'message'=>'通知公告修改成功！')); 
             }else{
                 $this->ajaxReturn(array('status'=>0, 'message'=>'通知公告修改失败！')); 
             }
         }else{//
-            $map = array('id' => $id);
-            $notice=M('Xx_article');
-            $result = $notice->where($map)->find();
-            $result['updatetime']=date("Y-m-d",$result['updatetime']);
-            $this->assign('affiche',$result);
+            $result = $this->newsList->showOneNews($id);
+            $this->assign('result',$result);
             $this->display();
         }
 	}
 
 	//删除行业资讯
     public function delNews(){
-        $data = I();
-        if (!$data['id']) {
+        $id = I('id');
+        if (!$id) {
             $this->error('id错误！');
         }
-        $where['id'] = I('id');
-        $affiche=M('Xx_article');;
-        $info = $affiche->delete($data['id']);
+        $info = $this->newsList->delete($id);
         if ($info == '0') {
             $this->success('不存在该数据！');
         }else if($info == false){
