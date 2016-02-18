6e91bc163495f0398fc3044c0045f53ab3e71817
tophgg <395886828@qq.com>
2016-02-18 17:08:51 +0800

    hotel的最新资讯

--- hotel/Hotel/Admin/Model/NewsListModel.class.php
+++ hotel/Hotel/Admin/Model/NewsListModel.class.php
@@ -2,18 +2,58 @@
 namespace Admin\Model;
 use Think\Model;
 class NewsListModel extends Model {
+    protected $tableName = 'news_list'; 
 	// 定义自动验证
     protected $_validate = array(
     	array('title','require','请输入标题'),
         array('details','require','新闻详情必须填写！'),
     );
+
     //获取新闻列表 
     public function showNews(){
     	$result = $this->where()->order('update_time desc')->select();
-    	if(count($result) < 0){
+    	if(count($result) <= 0){
     		return false;
     	}else{
     		return $result;
     	}
     }
+
+    //添加新闻 
+    public function addN($data){
+        if($data){
+            $data['update_time'] =date("Y-m-d",time());
+            $result = $this->add($data);
+            // print_r($result);exit();
+            if($result){
+                return true;
+            }else{
+                return false;
+            }
+        } 
+    }
+
+    //编辑新闻 
+    public function editN($id,$data){
+        if($data){   
+            $data['update_time'] =date("Y-m-d",time());
+            $result = $this->where('id = '.$id)->save($data);
+            if($result){
+                return true;
+            }else{
+                return false;
+            }
+        } 
+    }
+
+    //显示某条新闻记录
+    public function showOneNews($id){
+        $where['id'] = $id; 
+        $result = $this->where($where)->find();
+        if(count($result) <= 0){
+            return false;
+        }else{
+            return $result;
+        }
+    } 
 }
